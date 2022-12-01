d2b1773fa55a2ac84709278c91eb18be06ed7508
ChillerDragon <ChillerDragon@gmail.com>
2022-12-01 20:00:30 +0100

    Not proud

--- 01.sh
+++ 01.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 tr '\n' ' ' < 01.txt | sed 's/  /\n/g' | sed 's/ /+/g' | sed 's/+$//g' | awk '{ print "echo $((" $0 "))" }' | sh | sort -n | tail -n 1
+tr '\n' ' ' < 01.txt | sed 's/  /\n/g' | sed 's/ /+/g' | sed 's/+$//g' | awk '{ print "echo $((" $0 "))" }' | sh | sort -n | tail -n 3 | tr '\n' '+' | sed 's/+$//g' | awk '{ print "echo $((" $0 "))" }' | sh
