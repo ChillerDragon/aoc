3252d6d804d137f671a58c4cb9c9886afd7366b2
ChillerDragon <ChillerDragon@gmail.com>
2022-12-03 09:35:21 +0100

    Don't let my life advisor see this

--- 03.sh
+++ 03.sh
@@ -8,7 +8,7 @@ do
 	get_prio["$l"]="$i"
 done
 
-declare -a prios
+prios=()
 while read -r item
 do
 	len="${#item}"
@@ -24,5 +24,22 @@ do
 		fi
 	done < <(echo "$cmp1" | grep -o . | sort -u)
 done < 03.txt
+
+echo "$(IFS=+; echo "$((${prios[*]}))")"
+
+prios=()
+while read -r g1 g2 g3
+do
+	[[ "$g1" == "" ]] && continue
+
+	while read -r t1
+	do
+		if echo "$g2" | grep -q "$t1" && echo "$g3" | grep -q "$t1"
+		then
+			prios+=("${get_prio["$t1"]}")
+			break
+		fi
+	done < <(echo "$g1" | grep -o . | sort -u)
+done < <(paste -d " " - - - < 03.txt)
 
 echo "$(IFS=+; echo "$((${prios[*]}))")"
